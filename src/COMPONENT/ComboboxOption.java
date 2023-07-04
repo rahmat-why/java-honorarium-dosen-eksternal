4b6fa82a644ddb7798f180a55843cfba0b0d327c
rahmatwahyubudiyanto <rahmatwahyubudiyanto@gmail.com>
2023-07-04 07:11:48 +0700

    get helper

--- src/COMPONENT/ComboboxOption.java
+++ src/COMPONENT/ComboboxOption.java
@@ -3,10 +3,17 @@ package COMPONENT;
 public class ComboboxOption {
     private String value;
     private String display;
+    private String helper;
 
     public ComboboxOption(String value, String display) {
         this.value = value;
         this.display = display;
+    }
+
+    public ComboboxOption(String value, String display, String helper) {
+        this.value = value;
+        this.display = display;
+        this.helper = helper;
     }
 
     public String getValue() {
@@ -20,5 +27,9 @@ public class ComboboxOption {
 
     public String getDisplay() {
         return display;
+    }
+
+    public String getHelper() {
+        return helper;
     }
 }
